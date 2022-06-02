425d5c431a6d292cd51f7c777f812d434306bf1e
hem_b <hemanthb93@gmail.com>
2022-06-02 14:00:22 +0530

    [Hemanth]:Adds set of words to hash table and counts its frequeny

---
+++ HashTable/HashTable/MyMapNode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class MyMapNode<K, V>
+    {
+        public int size;
+        public LinkedList<KeyValue<K, V>>[] hashMap;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.hashMap = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        //Generates a hashcode based on the key which acts as an array position
+        public int GetArrayPosition(K key)
+        {
+            int pos = key.GetHashCode() % this.size;
+            return Math.Abs(pos);
+        }
+
+        //Adds a key value pair into hashtable
+        public void Add(K key,V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K,V>> list = GetLinkedList(position);
+            KeyValue<K,V> items = new KeyValue<K, V> { Key=key, Value=value };
+            list.AddLast(items);
+        }
+
+        // Method to Get Linked List
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = hashMap[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                hashMap[position] = linkedList;
+            }
+            return linkedList;
+        }
+        //Calculates the frequency of a given word 
+        public int GetFrequencyOfWords(V word)
+        {
+            int count = 0;
+            if(hashMap == null)
+            {
+                Console.WriteLine("hashTable is empty!!");
+                return 0;
+            }
+            for(int i = 0; i < hashMap.Length; i++)
+            {
+                LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(i);
+                foreach(KeyValue < K, V > item in linkedList)
+                {
+                    if (item.Value.Equals(word)) { count++; }
+                        
+                }
+            }
+            return count;
+        }
+        
+        
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+    }
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,13 @@
+﻿HashTable.MyMapNode<string, string> hash = new HashTable.MyMapNode<string, string>(6);
+hash.Add("0", "to");
+hash.Add("1", "be");
+hash.Add("2", "or");
+hash.Add("3", "not");
+hash.Add("4", "to");
+hash.Add("5", "be");
+
+Console.WriteLine("Frequency of word \'to\' is  : " + hash.GetFrequencyOfWords("to"));
+Console.WriteLine("Frequency of word \'be\' is  : " + hash.GetFrequencyOfWords("be"));
+Console.WriteLine("Frequency of word \'or\' is  : " + hash.GetFrequencyOfWords("or"));
+Console.WriteLine("Frequency of word \'not\' is : " + hash.GetFrequencyOfWords("not"));
+
